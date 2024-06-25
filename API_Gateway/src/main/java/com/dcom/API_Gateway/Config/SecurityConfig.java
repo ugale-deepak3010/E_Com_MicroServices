2c027f73a98161eec287192c09421840245f0199
Deepak Ugale <ugale.deepak3010@gmail.com>
2024-06-25 18:23:04 +0530

    Key clock implemented.

---
+++ API_Gateway/src/main/java/com/dcom/API_Gateway/Config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.dcom.API_Gateway.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+	
+	
+	@SuppressWarnings("removal")
+	@Bean
+	SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity serverHttpSecurity ) {
+
+        serverHttpSecurity.csrf(csrf -> csrf
+                .disable()
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt));
+		
+		
+		return serverHttpSecurity.build();
+	}
+
+}
